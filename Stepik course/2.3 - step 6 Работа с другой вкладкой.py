f4d390a4cabeb1648ef243831b715021b40aefe2
ismailtester <ismailtester@yandex.ru>
2023-10-08 22:14:27 +0300

    Added few of my latest tests

---
+++ Stepik course/2.3 - step 6 Работа с другой вкладкой.py
@@ -0,0 +1,55 @@
+import datetime
+
+import action as action
+import pyperclip as pyperclip
+from selenium import webdriver
+import time
+
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+link = "http://suninjuly.github.io/redirect_accept.html"
+
+import math
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+
+try:
+    browser = webdriver.Chrome()
+    browser.maximize_window()  # окно на весь экран
+    browser.get(link)
+    action = ActionChains(browser)
+    joinbutton = browser.find_element(By.CSS_SELECTOR, '[type = "submit"').click()
+    new_window = browser.window_handles[1] #Находим второе окно. Указана 1, но первое окно это 0
+    browser.switch_to.window(new_window) #Переходимо ко второму окну
+    x = WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.ID, "input_value"))).text
+    y = calc(x)  # Нужный расчет
+    answer = browser.find_element(By.ID, "answer")
+    answer.send_keys(y)
+    submitbutton = browser.find_element(By.CSS_SELECTOR, '.btn.btn-primary').click()
+    #Копирование кода в буфер обмена
+    alert = browser.switch_to.alert.text
+    addToClipBoard = alert.split(': ')[-1]
+    pyperclip.copy(addToClipBoard)
+    print("Тест пройден")
+
+
+
+
+
+
+
+
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(5)
+    browser.quit()
---
+++ Stepik course/2.4 - step 3  Использование webwaits.py
@@ -0,0 +1,37 @@
+import datetime
+
+import action as action
+from selenium import webdriver
+import time
+
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+link = "http://suninjuly.github.io/wait1.html"
+
+import math
+
+
+
+try:
+    browser = webdriver.Chrome()
+    browser.maximize_window()  # окно на весь экран
+    browser.get(link)
+    verifybutton = WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.ID, 'verify')))
+    verifybutton.click()
+    message = WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.ID, "verify_message")))
+    assert "successful" in message.text #Смотрим частичное совпаданиe сообщения с текстом
+
+
+
+
+
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(5)
+    browser.quit()
